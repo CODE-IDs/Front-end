113fd32884429efec14d297ccd41636bb59d798d
Photenix <juanmanuelpinor@gmail.com>
2023-09-09 14:28:43 -0500

    education

--- src/assets/Education/Education.jsx
+++ src/assets/Education/Education.jsx
@@ -3,25 +3,25 @@ import EducationCard from "./EducationCard"
 const Education = () => {
     return (
         <div className="education">
-            <h2 className="font-medium">Educación</h2>
+            <h2 className="font-medium text-6xl">Educación</h2>
             <div>
                 <h3 className="font-medium">Aprende ahora</h3>
                 <p>Aprende a ahorrar y manejar mejor tus finanzas</p>
             </div>
 
             <div className="flex flex-col items-center gap-1">
-                <div className="principal-card border-greenM border w-5/6 bg-white">
+                <div className="principal-card border w-11/12 bg-white">
                     <img src="https://img.freepik.com/vector-premium/ilustracion-vector-luz-bombilla_516670-384.jpg?w=2000" alt="w" 
                     className="w-[200px]"/>
                     <div>
                         <h3>title</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate rem, dolore aut alias fugiat vero expedita doloremque. Hic, quasi laudantium vitae, dolor placeat molestiae incidunt officiis ipsum nostrum odit sapiente.</p>
                         <div className="flex item-left justify-end ">
-                            <button className="rounded-lg p-6 bg-greenM text-white font-medium">Leer</button>
+                            <button className="rounded-lg p-4 bg-greenM text-white font-medium w-3/12">Leer</button>
                         </div>
                     </div>
                 </div>
-                <div className="w-5/6 flex gap-1">
+                <div className="w-11/12 flex gap-1">
                     <EducationCard title="Onomatopeya"/>
                     <EducationCard/>
                 </div>
