9e35bc1abf47ee9749ab61a495eef10e8f13f5d0
Luke Salazar <lucassy07@outlook.com>
2023-09-09 13:40:46 -0500

    Calculator logic working

---
+++ src/DB/EducationDB.js
@@ -0,0 +1,10 @@
+/* export default function educationDB() {
+  const DB = [''];
+  return (
+    DB
+  )
+}; */
+
+import getRecommendation from "./CalculatorLogic.js";
+
+console.log(getRecommendation(5550, 5440));
