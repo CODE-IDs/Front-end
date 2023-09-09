4692e182ebb16c43a5226368ab17400121b6ee53
nuthelk <nuthelkcd@gmail.com>
2023-09-09 17:00:07 -0500

    fix

--- src/Index.jsx
+++ src/Index.jsx
@@ -5,7 +5,7 @@ const Index = () => {
    
   return (
     <div className="flex flex-col items-center justify-center text-sm  bg-gray-100 md:text-lg min-h-screen ">
-      <p className="font-sans hover:font-serif mb-4 text-[40px]">{"CODE_ID'S"}</p>
+      <p className="font-sans hover:font-serif mb-4 text-[40px]">{"ZUMA"}</p>
       <a href="/calculadora"><button 
           className="bg-greenM hover:bg-Emerald-700 text-white font-bold py-2 px-4 rounded m-2 w-48">
           Calculadora Ahorros
