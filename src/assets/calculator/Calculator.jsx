2cb12fbaaa3872571cfd63b5ce06b1536ac19df9
nuthelk <nuthelkcd@gmail.com>
2023-09-09 12:52:46 -0500

    add: calculator

--- src/assets/calculator/Calculator.jsx
+++ src/assets/calculator/Calculator.jsx
@@ -1,13 +1,15 @@
-
-
 const Calculator = () => {
   return (
-    <dive className="w-screen ">
-        <h1>Calculator</h1>
-        <button className="rounded-lg p-6 bg-fourty text-white font-medium">get ready</button>
-        <input type="text" />
+    <dive className="w-screen  h-auto">
+      <div className="p-10 w-full m-auto flex flex-col items-center">
+        <h1 className="text-[150px] font-bold text-third">Calculator</h1>
+        {/* <button className="rounded-lg p-6 bg-fourty text-white font-medium">
+          get ready
+        </button> */}
+        <input type="text" className="p-3 rounded-lg outline-none" />
+      </div>
     </dive>
-  )
-}
+  );
+};
 
-export default Calculator 
+export default Calculator;
