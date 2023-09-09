e9152412654debb95424faa14b0644eb7f58acc7
Photenix <juanmanuelpinor@gmail.com>
2023-09-09 15:29:38 -0500

    navigator

--- src/assets/Navigator.jsx
+++ src/assets/Navigator.jsx
@@ -4,11 +4,11 @@ const Navigator = () => {
     let isHome = window.location.pathname == "/"
 
     return (
-        <nav className="absolute">
+        <nav className="">
             {
                 !isHome 
                 &&
-                <a href="/"><button className="p-4 text-fifty text-[30px] font-bold "> {"<"} </button></a>
+                <a href="/"><button className="text-center bottom-0 left-0 absolute text-fifty text-[30px] font-bold bg-[#ffffffc9] rounded-lg w-[70px] h-[70px]"> {"<"} </button></a>
             }
         </nav>
     )
